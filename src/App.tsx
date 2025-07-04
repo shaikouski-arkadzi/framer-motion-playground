c4d1bb8a712267c31aeb72a16cc45c6a4a69739a
Arkadzi_Shaikouski <arkadiy92@gmail.com>
2025-07-04 22:23:30 +0300

    Add motion for images

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,25 @@
+import { motion } from "framer-motion";
+import viteLogo from "/vite.svg";
+
 import "./App.css";
 
 function App() {
-  return <></>;
+  return (
+    <motion.img
+      src={viteLogo}
+      className="logo"
+      alt="Vite logo"
+      animate={{ rotate: 360 }}
+      transition={{
+        delay: 3,
+        duration: 2,
+        repeat: Infinity,
+        repeatDelay: 1,
+        repeatType: "reverse",
+        type: "spring",
+      }}
+    />
+  );
 }
 
 export default App;
