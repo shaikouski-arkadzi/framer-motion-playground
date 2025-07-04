322e62461bf530bc882cf10a7e757694bc8d7f26
Arkadzi_Shaikouski <arkadiy92@gmail.com>
2025-07-04 22:31:47 +0300

    Add animation on hover for text

--- src/App.tsx
+++ src/App.tsx
@@ -5,20 +5,25 @@ import "./App.css";
 
 function App() {
   return (
-    <motion.img
-      src={viteLogo}
-      className="logo"
-      alt="Vite logo"
-      animate={{ rotate: 360 }}
-      transition={{
-        delay: 3,
-        duration: 2,
-        repeat: Infinity,
-        repeatDelay: 1,
-        repeatType: "reverse",
-        type: "spring",
-      }}
-    />
+    <>
+      <motion.img
+        src={viteLogo}
+        className="logo"
+        alt="Vite logo"
+        animate={{ rotate: 360 }}
+        transition={{
+          delay: 3,
+          duration: 2,
+          repeat: Infinity,
+          repeatDelay: 1,
+          repeatType: "reverse",
+          type: "spring",
+        }}
+      />
+      <motion.p whileHover={{ color: "red" }}>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+      </motion.p>
+    </>
   );
 }
 
